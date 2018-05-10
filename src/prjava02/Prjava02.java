3018948df4a2ff3ba18c64d61c5858bb2c66a686
jelamu1718dawvib <jelamu1718dawvib@gmail.com>
2018-05-10 08:49:16 +0200

    Treballant amb ordres Git i branques: Versió 1.2 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -16,6 +16,7 @@ public class Prjava02 {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         System.out.println("versió 0.1 del projecte prjava02"); 	
         try {
